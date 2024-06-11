69c229ce011cc1489be45210af1122a99e9f8feb
MATHEUS CANTARINO TINTI <matheus.ctinti@senacsp.edu.br>
2024-06-11 11:52:39 -0300

    Inicio do login no Python

---
+++ sistema.py
@@ -0,0 +1,7 @@
+""" 
+    Este arquivo é a entrada do sistema, ele aciona os módulos necessários.
+"""
+# Importação do Módulo Usuario e da classe
+from Modulos.Usuario import Usuario
+
+usuario = Usuario()
