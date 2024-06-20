b89b2a10389b8ddce6148f62190a07a678ce5311
MATHEUS CANTARINO TINTI <matheus.ctinti@senacsp.edu.br>
2024-06-20 11:50:36 -0300

    Testes com Json

--- Modulos/GravaDados.py
+++ Modulos/GravaDados.py
@@ -1,12 +1,12 @@
 import builtins
-
+import json
 class GravaDados:
     
-    usuarioCadastrados = {
-     "loginArmazenado" : "cliente",
+    usuariosCadastrados = {
+    "loginArmazenado" : "cliente",
     "senhaArmazenada" : "1234",
     "nomeUsuario" : "João dos Santos",
-  }
+    }
     
     def salvaDados( self ):
         #open( nomeArquivo,modoAbertuta) é uma função do python3 que lê e escreve arquivos
@@ -14,15 +14,17 @@ class GravaDados:
         # r - read - leitura
         # r+ - escrita/leitura
         
-        with open("Lanchonete-python\\Modulos\\dados.txt","w") as dados:   
-            dados.write(self.usuarioCadastrados)
+        with open("lanchonete-python\\Modulos\\dados.txt","w") as dados:   
+            # converter o vetor em json
+            dadosJson = json.dumps( self.usuariosCadastrados)
+            dados.write( dadosJson)
             dados.close()
             print("Dados armazenado")  
         
             
     def leDados( self ):
         # para ler usamos a classe builtins do python3
-        with builtins.open("Lanchonete-python\\Modulos\\dados.txt","r") as dados: 
+        with builtins.open("lanchonete-python\\Modulos\\dados.txt","r") as dados: 
                 
             print(f"No arquivo temos: \n ")
             
@@ -30,10 +32,23 @@ class GravaDados:
             # para cada linha do arquivo de texto 
         for linha in dados.readlines() :
                 print( linha.rstrip() )
-                
+   
+    def adicionaCadastro( self,novoUsuario ):
+        # pegamos o json no arquivo de dados
+        with builtins.open("lanchonete-python\\Modulos\\dados.txt","r") as dados:
+            
+        # adicionado um novo item ao nosso vetor 
+    
+        
+        # salvar novamente o arquivo
+        
+        
+        self.usuariosCadastrados = self.usuariosCadastrados.append( novoUsuario )
+            
     def exportaDados( self ):
         print("")
         
-dados = GravaDados()      
+dados = GravaDados()
+dados.adicionaCadastro()      
 dados.salvaDados()      
-dados.LeDados()      
+dados.leDados()      
